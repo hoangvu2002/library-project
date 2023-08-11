8845b71ba55cf22347c0db2e6f1f3db187fb7b91
pdhoangvu <pdhoangvu@gmail.com>
2023-08-12 01:03:39 +0700

    Define the constructor and addBookToLibrary function

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const form = document.querySelector("form")
 form.onsubmit = (e) => {e.preventDefault();}
 
+let myLibrary = [];
+
+function Book(title, author, pages, read=false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const read = document.getElementById('read');
+  const book = new Book(title.value, author.value, pages.value, read.checked);
+  myLibrary.push(book);
+  title.value = "";
+  author.value = "";
+  pages.value="";
+  read.checked=false;
+}
